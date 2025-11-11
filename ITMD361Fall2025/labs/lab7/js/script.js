431f401f25a8a878d3b13771170826e0e013c0ef
yayo5576 <ymartinez2@hawk.illinoistech.edu>
2025-11-11 17:22:05 -0600

    final full commit

--- ITMD361Fall2025/labs/lab7/js/script.js
+++ ITMD361Fall2025/labs/lab7/js/script.js
@@ -1,17 +1,14 @@
 function init(){
 //add your javascrip between these two lines of code
-  document.getElementById("entrybutton").addEventListener("click", myAlert); 
+   document.getElementById("entrybutton").addEventListener("click", myAlert); 
 
-
-  function myAlert(){
-   var inputV = document.getElementById('entryinput'); 
-   var valueInput = inputV.value; 
-   alert ("Yael Martinez: " + valueInput);
-   document.getElementById('textoutput').innerText = `${valueInput}`;
-
+   function myAlert(){
+     var inputV = document.getElementById('entryinput'); 
+     var valueInput = inputV.value; 
+     alert ("Yael Martinez: " + valueInput);
+     document.getElementById('textoutput').innerText = `${valueInput}`;
   }
   
-
 }
 window.addEventListener('load', init);
 
