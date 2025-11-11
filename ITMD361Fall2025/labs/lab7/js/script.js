ed6eb5cb18691cf098e9ff73601fe6bdb487fe58
yayo5576 <ymartinez2@hawk.illinoistech.edu>
2025-11-11 17:07:44 -0600

    Full Commit

---
+++ ITMD361Fall2025/labs/lab7/js/script.js
@@ -0,0 +1,17 @@
+function init(){
+//add your javascrip between these two lines of code
+  document.getElementById("entrybutton").addEventListener("click", myAlert); 
+
+
+  function myAlert(){
+   var inputV = document.getElementById('entryinput'); 
+   var valueInput = inputV.value; 
+   alert ("Yael Martinez: " + valueInput);
+   document.getElementById('textoutput').innerText = `${valueInput}`;
+
+  }
+  
+
+}
+window.addEventListener('load', init);
+
